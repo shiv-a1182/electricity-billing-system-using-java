8c5fdb42b868457024d07ce9a87faa27b26d485d
Shiva sai Gundeti <125118911+shiv-a1182@users.noreply.github.com>
2023-06-03 15:57:52 +0530

    ebs

--- p.java
+++ p.java
@@ -1,5 +1,22 @@
-package p1;
-public class p
+package billing;                                                                              //PACKAGE
+import java.util.*;  
+public class  transaction
 {
-    JLabel l1=new JLabel('1.png');
-}
+	Scanner sc = new Scanner(System.in); 
+   double bill;
+ public void bills()                                                      //ACCESS MODIFIER (PUBLIC)
+                                                                                                    //OVER LOADING
+ {
+	  System.out.println("Enter your userid:");
+	int uid=sc.nextInt();
+	System.out.println("Enter the bill to be paid");
+	 bill=sc.nextDouble();
+ System.out.println("Enter your payment mode 1.credit card 2.Debit card 3.UPI:");
+	int mode=sc.nextInt();
+	System.out.println("Enter 1. To pay  2. To pay later:");
+	int i=sc.nextInt();
+System.out.println("The electricity bill of : " + bill+"has been paid succesfully");   
+	return;
+ }
+
+
